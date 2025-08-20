f07c0bf2626a256b238bb6e5dd20ff55bd6dc5c6
chiragdotjain <chiragdotjain@google.com>
2025-08-20 13:26:56 +0000

    restructure dirs

---
+++ src/main/java/mainApp/ApiController.java
@@ -0,0 +1,19 @@
+package mainApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
